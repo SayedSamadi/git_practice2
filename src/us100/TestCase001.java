bdb02c1342b8ba05f1806a8a0bc33382f891c1b3
AhmadS <samadigithub@gmail.com>
2022-01-11 08:23:15 -0800

    First work is done

--- src/us100/TestCase001.java
+++ src/us100/TestCase001.java
@@ -1,4 +1,7 @@
 package us100;
 
 public class TestCase001 {
+    public static void main(String[] args) {
+        System.out.println("This is to confirm that the first project was shared");
+    }
 }
